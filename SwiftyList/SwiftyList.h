df4c62a567dbff2a128304cd0e682a81af9cabac
Brychan Odlum <git@brychan.io>
2019-01-29 20:55:05 -0800

    Init

---
+++ SwiftyList/SwiftyList.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyList.h
+//  SwiftyList
+//
+//  Created by Brychan Bennett-Odlum on 29/01/2019.
+//  Copyright © 2019 Brychan Bennett-Odlum. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftyList.
+FOUNDATION_EXPORT double SwiftyListVersionNumber;
+
+//! Project version string for SwiftyList.
+FOUNDATION_EXPORT const unsigned char SwiftyListVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyList/PublicHeader.h>
+
+
---
+++ SwiftyList/SwiftyList.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyList.swift
+//  SwiftyList
+//
+//  Created by Brychan Bennett-Odlum on 29/01/2019.
+//  Copyright © 2019 Brychan Bennett-Odlum. All rights reserved.
+//
+
+import Foundation
+
+public class SwiftyList {
+	static let shared = SwiftyList()
+	
+	func foo() {
+		
+	}
+}
