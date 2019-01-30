1f015656b57caf66ba38e458813221fba7193231
Brychan Odlum <git@brychan.io>
2019-01-29 22:53:05 -0800

    List view init

---
+++ SwiftyList/Views/SwiftyListView.swift
@@ -0,0 +1,13 @@
+//
+//  SwiftyListView.swift
+//  SwiftyList
+//
+//  Created by Brychan Bennett-Odlum on 29/01/2019.
+//  Copyright © 2019 Brychan Bennett-Odlum. All rights reserved.
+//
+
+import Foundation
+
+class ChatViewController: NSViewController {
+	
+}
